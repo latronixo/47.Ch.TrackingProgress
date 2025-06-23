875552a305fed5ff39ae0149f8fbe5984fd1c992
Valentin <latronixo@gmail.com>
2025-06-23 15:14:00 +0300

    Initial Commit

---
+++ 47.Ch.TrackingProgress/_7_Ch_TrackingProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_Ch_TrackingProgressApp.swift
+//  47.Ch.TrackingProgress
+//
+//  Created by Валентин on 23.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct _7_Ch_TrackingProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
